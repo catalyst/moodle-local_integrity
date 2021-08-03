3c717402c9ea3e794c02edaebe43ff4290eb8879
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2021-08-03 11:54:58 +1000

    Add callbacks to extend mod form

---
+++ lib.php
@@ -0,0 +1,104 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Callbacks.
+ *
+ * @package     local_activity_notifications
+ * @copyright   2021 Catalyst IT
+ * @author      Dmitrii Metelkin (dmitriim@catalyst-au.net)
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use local_activity_notifications\notification_factory;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Extend course module form.
+ *
+ * @param \moodleform_mod $modform Mod form instance.
+ * @param \MoodleQuickForm $form Form instance.
+ */
+function local_activity_notifications_coursemodule_standard_elements(moodleform_mod $modform, MoodleQuickForm $form): void {
+    $cm = $modform->get_coursemodule();
+    $modname = '';
+
+    // Coerce modname from course module if we are updating existing module.
+    if (!empty($cm) && !empty($cm->modname)) {
+        $modname = $cm->modname;
+    } else if (!empty($modform->get_current()->modulename)) {
+        $modname = $modform->get_current()->modulename;
+    }
+
+    if (!empty($modname)) {
+        $notification = notification_factory::get_notification($modname);
+        if (!empty($notification)) {
+            $notification->coursemodule_standard_elements($modform, $form);
+        }
+    }
+}
+
+/**
+ * Extend course module form submission.
+ *
+ * @param \stdClass $moduleinfo Module info data.
+ * @param \stdClass $course Course instance.
+ *
+ * @return \stdClass Mutated module info data.
+ */
+function local_activity_notifications_coursemodule_edit_post_actions(stdClass $moduleinfo, stdClass $course): stdClass {
+    $modname = $moduleinfo->modulename;
+
+    if (!empty($modname)) {
+        $notification = notification_factory::get_notification($modname);
+        if (!empty($notification)) {
+            $moduleinfo = $notification->coursemodule_edit_post_actions($moduleinfo, $course);
+        }
+    }
+
+    return $moduleinfo;
+}
+
+/**
+ * Extend course mod form validation.
+ *
+ * @param \moodleform_mod $modform Mod form instance.
+ * @param array $data Submitted data.
+ *
+ * @return array
+ */
+function local_activity_notifications_coursemodule_validation(moodleform_mod $modform, array $data): array {
+    $errors = [];
+
+    $cm = $modform->get_coursemodule();
+    $modname = '';
+
+    if (!empty($cm) && !empty($cm->modname)) {
+        $modname = $cm->modname;
+    } else if (!empty($modform->get_current()->modulename)) {
+        $modname = $modform->get_current()->modulename;
+    }
+
+    if (!empty($modname)) {
+        $notification = notification_factory::get_notification($modname);
+        if (!empty($notification)) {
+            $errors = $notification->coursemodule_validation($modform, $data);
+        }
+    }
+
+    return $errors;
+}
