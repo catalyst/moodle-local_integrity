fdb685eec4af3b39eaadba7e565efc367164a5f1
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2021-08-06 15:53:51 +1000

    Add user data class

---
+++ classes/userdata.php
@@ -0,0 +1,150 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Class containing user data.
+ *
+ * @package     local_integrity
+ * @copyright   2021 Catalyst IT
+ * @author      Dmitrii Metelkin (dmitriim@catalyst-au.net)
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_integrity;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class containing user data.
+ *
+ * @package     local_integrity
+ * @copyright   2021 Catalyst IT
+ * @author      Dmitrii Metelkin (dmitriim@catalyst-au.net)
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class userdata {
+
+    /**
+     * Table name.
+     */
+    const TABLE = 'local_integrity_userdata';
+
+    /**
+     * @var
+     */
+    private $id = null;
+
+    /**
+     * User id.
+     * @var int
+     */
+    private $userid;
+
+    /**
+     * Plugin name.
+     * @var string
+     */
+    private $plugin;
+
+    /**
+     * A list of context id for that user.
+     * @var array
+     */
+    private $contextids = [];
+
+    /**
+     * Constructor.
+     *
+     * @param int $userid User ID.
+     * @param string $plugin Plugin name.
+     *
+     * @throws \dml_exception
+     */
+    public function __construct(int $userid, string $plugin) {
+        global $DB;
+
+        $this->userid = $userid;
+        $this->plugin = $plugin;
+
+        $record = $DB->get_record(self::TABLE, ['userid' => $this->userid, 'plugin' => $this->plugin]);
+        if (!empty($record)) {
+            $this->id = $record->id;
+            $this->contextids = json_decode($record->contextids);
+        }
+    }
+
+    /**
+     * Return a list of context ids for the user for the plugin,
+     * @return array
+     */
+    public function get_context_ids(): array {
+        return $this->contextids;
+    }
+
+    /**
+     * Add context ID to the list.
+     *
+     * @param int $contextid
+     */
+    public function add_context_id(int $contextid) {
+        if (!in_array($contextid, $this->contextids)) {
+            $this->contextids[] = $contextid;
+            $this->save();
+        }
+    }
+
+    /**
+     * Remove context ID from the list.
+     *
+     * @param int $contextid
+     */
+    public function remove_context_id(int $contextid) {
+        if (($key = array_search($contextid, $this->contextids)) !== false) {
+            unset($this->contextids[$key]);
+            $this->save();
+        }
+    }
+
+    /**
+     * Check if the provided context ID exists in  the list.
+     * @param int $contextid
+     *
+     * @return bool
+     */
+    public function is_context_id_exist(int $contextid): bool {
+        return in_array($contextid, $this->contextids);
+    }
+
+    /**
+     * Save data to DB.
+     */
+    protected function save() {
+        global $DB;
+
+        $data = new \stdClass();
+        $data->userid = $this->userid;
+        $data->plugin = $this->plugin;
+        $data->contextids = json_encode($this->contextids);
+
+        if (empty($this->id)) {
+            $this->id = $DB->insert_record(self::TABLE, $data);
+        } else {
+            $data->id = $this->id;
+            $DB->update_record(self::TABLE, $data);
+        }
+    }
+
+}
---
+++ tests/userdata_test.php
@@ -0,0 +1,118 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for userdata class.
+ *
+ * @package     local_integrity
+ * @copyright   2021 Catalyst IT
+ * @author      Dmitrii Metelkin (dmitriim@catalyst-au.net)
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_integrity\tests;
+
+use advanced_testcase;
+use local_integrity\userdata;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Tests for userdata class.
+ *
+ * @package     local_integrity
+ * @copyright   2021 Catalyst IT
+ * @author      Dmitrii Metelkin (dmitriim@catalyst-au.net)
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class userdata_test extends advanced_testcase {
+
+    /**
+     * Test empty data.
+     */
+    public function test_empty_data() {
+        $userdata = new userdata(1, 'test');
+        $this->assertCount(0, $userdata->get_context_ids());
+        $this->assertFalse($userdata->is_context_id_exist(rand()));
+        $userdata->remove_context_id(2);
+        $this->assertCount(0, $userdata->get_context_ids());
+    }
+
+    /**
+     * Test can add and delete.
+     */
+    public function test_can_add_and_delete() {
+        $this->resetAfterTest();
+
+        $userdata = new userdata(1, 'test');
+        $this->assertCount(0, $userdata->get_context_ids());
+        $this->assertFalse($userdata->is_context_id_exist(rand()));
+
+        $userdata->add_context_id(50);
+        $userdata->add_context_id(51);
+        $userdata->add_context_id(55);
+
+        $this->assertCount(3, $userdata->get_context_ids());
+        $this->assertTrue($userdata->is_context_id_exist(50));
+        $this->assertTrue($userdata->is_context_id_exist(51));
+        $this->assertTrue($userdata->is_context_id_exist(55));
+
+        $userdata = new userdata(1, 'test');
+        $this->assertCount(3, $userdata->get_context_ids());
+        $this->assertTrue($userdata->is_context_id_exist(50));
+        $this->assertTrue($userdata->is_context_id_exist(51));
+        $this->assertTrue($userdata->is_context_id_exist(55));
+
+        $userdata->remove_context_id(50);
+        $this->assertCount(2, $userdata->get_context_ids());
+        $this->assertFalse($userdata->is_context_id_exist(50));
+        $this->assertTrue($userdata->is_context_id_exist(51));
+        $this->assertTrue($userdata->is_context_id_exist(55));
+
+        $userdata->remove_context_id(51);
+        $this->assertFalse($userdata->is_context_id_exist(50));
+        $this->assertFalse($userdata->is_context_id_exist(51));
+        $this->assertTrue($userdata->is_context_id_exist(55));
+
+        $userdata->remove_context_id(55);
+        $this->assertFalse($userdata->is_context_id_exist(50));
+        $this->assertFalse($userdata->is_context_id_exist(51));
+        $this->assertFalse($userdata->is_context_id_exist(55));
+        $this->assertCount(0, $userdata->get_context_ids());
+
+        $userdata = new userdata(1, 'test');
+        $this->assertCount(0, $userdata->get_context_ids());
+    }
+
+    /**
+     * Test can't add more than once.
+     */
+    public function test_can_not_add_more_than_one_time() {
+        $this->resetAfterTest();
+
+        $userdata = new userdata(1, 'test');
+        $this->assertCount(0, $userdata->get_context_ids());
+        $this->assertFalse($userdata->is_context_id_exist(rand()));
+
+        $userdata->add_context_id(50);
+        $userdata->add_context_id(50);
+        $this->assertCount(1, $userdata->get_context_ids());
+
+        $userdata = new userdata(1, 'test');
+        $this->assertCount(1, $userdata->get_context_ids());
+    }
+
+}
