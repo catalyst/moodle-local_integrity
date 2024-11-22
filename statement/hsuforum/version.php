42e933893dfdae77c9d9ee3a3d74bf96004a4d6d
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2024-11-22 13:53:06 +1100

    issue #37: bump versions for all subplugins

--- statement/hsuforum/version.php
+++ statement/hsuforum/version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'integritystmt_hsuforum';
-$plugin->release = '0.1.0';
-$plugin->version = 2021072308;
-$plugin->requires = 2020061500;
+$plugin->release = 2024112200;
+$plugin->version = 2024112200;
+$plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_STABLE;
